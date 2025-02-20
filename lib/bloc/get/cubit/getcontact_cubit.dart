c6d2298a8606fae721ffd1980eebfd716b8fb624
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2025-02-20 14:01:05 +0630

    get method in cubit.dart for contact

--- lib/bloc/get/cubit/getcontact_cubit.dart
+++ lib/bloc/get/cubit/getcontact_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_rest_api/data/contact_repository.dart';
+import 'package:bloc_rest_api/data/model/contact.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'getcontact_state.dart';
 
 class GetcontactCubit extends Cubit<GetcontactState> {
-  GetcontactCubit() : super(GetcontactInitial());
+  final ContactRepository _contactRepository;
+  GetcontactCubit(this._contactRepository) : super(GetcontactInitial());
+
+  void getContact(){
+    emit(GetcontactInitial());
+    _contactRepository.getContact()
+    .then((value) => emit(GetcontactSuccess(value)))
+    .catchError((e) => emit(GetcontactFail('error')));
+  }
 }
