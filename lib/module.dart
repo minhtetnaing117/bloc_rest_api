12ee69089d6b5c2ec35282c3a335effd895ac601
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2025-02-20 13:46:49 +0630

    add module for locator to use

---
+++ lib/module.dart
@@ -0,0 +1,16 @@
+import 'package:bloc_rest_api/data/api/apiservice.dart';
+import 'package:bloc_rest_api/data/contact_repository.dart';
+import 'package:get_it/get_it.dart';
+import 'package:dio/dio.dart';
+
+var getIt = GetIt.I;
+void locator(){
+  Dio dio = Dio();
+  getIt.registerLazySingleton(() => dio);
+
+  ApiService apiService = ApiService(getIt.call());
+  getIt.registerLazySingleton(() => apiService);
+
+  ContactRepository contactRepository = ContactRepository(getIt.call());
+  getIt.registerLazySingleton(() => contactRepository);
+}
