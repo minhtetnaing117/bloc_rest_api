f9bedc90ed6d487ee33ecfa231dbeb28ab7a80f7
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2025-02-20 16:56:40 +0630

    add home.dart data and widget for tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:bloc_rest_api/bloc/get/cubit/getcontact_cubit.dart';
 import 'package:bloc_rest_api/module.dart';
 import 'package:bloc_rest_api/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   locator();
@@ -13,7 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: BlocProvider<GetcontactCubit>(
+        create: (context) => getIt.call(),
+        child: Home(),
+      ),
     );
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,12 +1,39 @@
+import 'package:bloc_rest_api/bloc/get/cubit/getcontact_cubit.dart';
+import 'package:bloc_rest_api/data/model/contact.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    BlocProvider.of<GetcontactCubit>(context).getContact();
     return Scaffold(
       appBar: AppBar(title: Text('Contact List'),),
+      body: BlocBuilder<GetcontactCubit, GetcontactState>(
+        builder: (context, state) {
+          if(state is GetcontactSuccess){
+            List<Contact> contacts = state.contacts;
+            return ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context,position){
+                return item(contacts[position]);
+              }
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+  Widget item(Contact contact){
+    return Card(
+      child: ListTile(
+        title: Text(contact.name),
+        subtitle: Text(contact.job),
+        leading: Text(contact.age),
+      ),
     );
   }
 }
